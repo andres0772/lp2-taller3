49aa87dff6017d7704117940d16a22b366569f76
andres peña <andresesteb0772@gmail.com>
2025-03-31 19:47:47 -0500

    actualizacion main

--- main.py
+++ main.py
@@ -19,11 +19,32 @@ app = Flask(__name__)
 
 def descargar(url):
   df = pd.read_csv(url)
+  df['created_at'] = pd.to_datetime(df['created_at'])
+  if 'field6' in df.columns:
+  df.drop(['entry_id', 'field5', 'field6'], axis=1, implace=True)
+  else:
+    df.drop(['entry_id', 'field5', 'field7'], axis=1, implace=True)
+    #renombre de columnas
+    df.columns = ['fecha', 'temp_exterior', 'temp_interior', 'presion_atm','humedad']
+    return df
+
+def graficar(df):
+  for columna in df.columns
+
+  #creacion de la figure
+  fig = plt.figure(figsize==(8,5))
+  #se hace la grafica
+  plt.plot(df['fecha'], df[columna], label=columna)
+  #se pone los titulos
+  plt.tittle(f"historia sobre {columna}")
+  
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 # Programa Principal
-if __name__ == '__main__':   
+if __name__ == '__main__':
+      
   # Ejecuta la app
   app.run(host='0.0.0.0', debug=True)
